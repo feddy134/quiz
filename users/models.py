72ec8b1e7c1d864db5aa81cab402c8d6de89a90f
fedrick <fedrick@yodaplus.com>
2021-04-16 21:10:42 +0530

    python manage.py createsuperuser uddated

--- users/models.py
+++ users/models.py
@@ -60,7 +60,7 @@ class User(AbstractUser):
     user_display_name = models.CharField(max_length=255, null=True, blank=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.user_display_name
